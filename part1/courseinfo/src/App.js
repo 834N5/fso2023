5662c19b0a70483eb3538bbdd1d740ec7980edb1
834N5 <pkgduk@disroot.org>
2023-08-05 15:33:40 +1200

    first component for 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,41 @@
 import React from "react";
 
-function Hello(props) {
-	console.log(props);
-	return (
+function Header(props) {
+	return(
 		<>
-			<p>hi {props.name}, you are {props.age} years old</p>
+			<h1>{props.course}</h1>
 		</>
 	)
 }
 
+function Content() {
+}
+
+function Total() {
+}
+
 function App() {
-	console.log("Hello from component");
-	const now = new Date();
-	const a = 10;
-	const b = 20;
-	const name = "alice";
-	const age = 9;
+	const course = "Half Stack application development";
+	const part1 = "Fundamentals of React";
+	const exercises1 = 10;
+	const part2 = "Using props to pass data";
+	const exercises2 = 7;
+	const part3 = "State of a component";
+	const exercises3 = 14;
+
 	return (
 		<>
-			<p>Hello, it is: {now.toString()}</p>
-			<p>{a} + {b} = {a+b}</p>
-			<Hello name="person" age="99" />
-			<Hello name={name} age={age} />
+			<Header course={course} />
+			<p>
+				{part1} {exercises1}
+			</p>
+			<p>
+				{part2} {exercises2}
+			</p>
+			<p>
+				{part3} {exercises3}
+			</p>
+			<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
 		</>
 	);
 }
