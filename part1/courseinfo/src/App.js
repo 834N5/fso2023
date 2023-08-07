fcf71494a781c046cf8c999f1f259e363005bf74
834N5 <pkgduk@disroot.org>
2023-08-07 22:44:50 +1200

    unicafe

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 
 function Header(props)
 {
@@ -71,4 +71,3 @@ function App()
 }
 
 export default App;
-//
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<>
+			<h1>Unicafe</h1>
+		</>
+	)
+}
+
+export default App;
