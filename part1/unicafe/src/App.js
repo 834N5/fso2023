61b38170c701ec9cb09b5622ee4b0df062d61687
834N5 <pkgduk@disroot.org>
2023-08-07 22:57:48 +1200

    Start design

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,28 @@
+function Button() {
+	return (
+		<>
+			<button>big red button</button>
+		</>
+	)
+}
+
+function Stats() {
+	return (
+		<>
+			<p>something something stats</p>
+		</>
+	)
+}
 function App() {
 	return (
 		<>
 			<h1>Unicafe</h1>
+			<h1>Give feedback</h1>
+			<Button />
+			<Button />
+			<Button />
+			<h1>Statistics</h1>
+			<Stats />
 		</>
 	)
 }
