6d83fc504d3ef135b92030fa733ff95e3026f5a8
834N5 <pkgduk@disroot.org>
2023-08-07 23:26:09 +1200

    exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,28 +1,39 @@
-function Button({text}) {
+import {useState} from "react";
+
+function Button({text, handleClick}) {
 	return (
 		<>
-			<button>{text}</button>
+			<button onClick={handleClick}>{text}</button>
 		</>
 	)
 }
 
-function Stats() {
+function Stats({dataGood, dataNeutral, dataBad}) {
 	return (
 		<>
 			<p>something something stats</p>
+			<p>Good: {dataGood}</p>
+			<p>Neutral: {dataNeutral}</p>
+			<p>Bad: {dataBad}</p>
 		</>
 	)
 }
 function App() {
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
 	return (
 		<>
 			<h1>Unicafe</h1>
+
 			<h1>Give feedback</h1>
-			<Button text="good"/>
-			<Button text="neutral"/>
-			<Button text="bad"/>
+			<Button text="good" handleClick={() => setGood(good+1)} />
+			<Button text="neutral" handleClick={() => setNeutral(neutral+1)} />
+			<Button text="bad" handleClick={() => setBad(bad+1)} />
+
 			<h1>Statistics</h1>
-			<Stats />
+			<Stats dataGood={good} dataNeutral={neutral} dataBad={bad} />
 		</>
 	)
 }
