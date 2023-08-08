e0bca3139260ec86d4c1a133eedd64111f054e08
834N5 <pkgduk@disroot.org>
2023-08-08 22:47:23 +1200

    Exercise 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,8 +1,40 @@
+import { useState } from "react"
+
+function Button({text, handleClick}) {
+	return(
+		<>
+			<button onClick={handleClick}>{text}</button>
+		</>
+	);
+}
+
 function App() {
-  return (
-  	<>
-  	</>
-  );
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+		"The only way to go fast, is to go well."
+	];
+
+	const [selected, setSelected] = useState(0);
+
+	function selectRandom() {
+		let rand;
+		// Avoids selecting the same number again
+		while((rand=Math.floor(Math.random()*anecdotes.length)) == selected);
+		setSelected(rand);
+	}
+
+	return(
+	  	<>
+	  		<h1>{anecdotes[selected]}</h1>
+	  		<Button text="Next Anecdote" handleClick={selectRandom} />
+	  	</>
+	);
 }
 
 export default App;
