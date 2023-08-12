e7dce91da368e6d3fa426b6e21c249dd370a6e0b
834N5 <pkgduk@disroot.org>
2023-08-13 00:07:30 +1200

    Exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,45 @@
 import {useState} from "react"
 
-function App() {
+function People({people})
+{
 	return(
 		<>
-			<h1>ok</h1>
+			{people.map(people =>
+				<p key={people.id}>{people.name}</p>
+			)}
+		</>
+	);
+}
+function App()
+{
+	const [persons, setPersons] = useState([
+		{name: "Arto Hellas", id: 0}
+	]);
+	const [newName, setNewName] = useState("wow");
+
+	function addName(event)
+	{
+		event.preventDefault()
+		setPersons(persons.concat({name: newName, id: persons.length}));
+	}
+	function handleNameChange(event)
+	{
+		setNewName(event.target.value);
+	}
+
+	return(
+		<>
+			<h1>Phonebook</h1>
+			<form onSubmit={addName}>
+				<div>
+					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h1>Numbers</h1>
+			<People people={persons} />
 		</>
 	);
 }
