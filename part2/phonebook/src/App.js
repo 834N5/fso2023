1ee9ae92d2c17ff84bfacf0afe50e6111566c5fa
834N5 <pkgduk@disroot.org>
2023-08-13 23:59:07 +1200

    Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,7 +5,7 @@ function People({people})
 	return(
 		<>
 			{people.map(people =>
-				<p key={people.id}>{people.name}</p>
+				<p key={people.id}>{people.name} {people.number}</p>
 			)}
 		</>
 	);
@@ -13,35 +13,50 @@ function People({people})
 function App()
 {
 	const [persons, setPersons] = useState([
-		{name: "Arto Hellas", id: 0}
+		{name: "Arto Hellas", number: "1234567890", id: 0}
 	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
-	function addName(event)
+	function addNumber(event)
 	{
 		event.preventDefault()
 		let name = newName.trim()
+		let number = newNumber.trim()
 		setNewName(name);
+		setNewNumber(number);
 		if (!name)
 			alert("Name must not be empty");
-		else if (persons.some((person) => person.name === name.trim()))
+		else if (!number)
+			alert("Number must not be empty");
+		else if (persons.some((person) => person.name === name))
 			alert(`${name} has already been added to the phonebook.`);
+		else if (persons.some((person) => person.number === number))
+			alert(`The number: ${number} has already been used.`);
 		else {
-			setPersons(persons.concat({name: name.trim(), id: persons.length}));
+			setPersons(persons.concat({name: name, number: number, id: persons.length}));
 			setNewName("");
+			setNewNumber("");
 		}
 	}
 	function handleNameChange(event)
 	{
 		setNewName(event.target.value);
 	}
+	function handleNumberChange(event)
+	{
+		setNewNumber(event.target.value);
+	}
 
 	return(
 		<>
 			<h1>Phonebook</h1>
-			<form onSubmit={addName}>
+			<form onSubmit={addNumber}>
 				<div>
 					name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					number: <input value={newNumber} onChange={handleNumberChange} />
 				</div>
 				<div>
 					<button type="submit">add</button>
