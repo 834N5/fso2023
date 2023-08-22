e083bc50bccd8ea44494e6409ab61cc91a3a1d1f
834N5 <pkgduk@disroot.org>
2023-08-23 00:53:07 +1200

    Exercise 2.18

--- part2/countries/src/Results.jsx
+++ part2/countries/src/Results.jsx
@@ -1,13 +1,17 @@
 function Results({countries, query}) {
 	if (!countries)
 		return(<p>Getting data from server...</p>);
-	console.log(countries);
-	console.log(query);
-	console.log(countries[4]);
+
+	if (!query)
+		return(<p>Search for a country!</p>);
+
 	const countryResults = countries.filter((country, index) =>
 		country.name.common.toLowerCase().includes(query) ||
 		country.name.official.toLowerCase().includes(query)
 	);
+	if (countryResults.length > 10)
+		return(<p>Too many matches, please make your query more specific.</p>);
+
 	return(
 		<>
 			{countryResults.map((country, index)=> <p key={index}>{country.name.common}</p>)}
