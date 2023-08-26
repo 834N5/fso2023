f02c2f580a027a554eadf74a49463db9119fbf11
834N5 <pkgduk@disroot.org>
2023-08-26 22:32:33 +1200

    Exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -42,3 +42,12 @@ app.get("/persons", (request, response) => {
 	console.log(request);
 	response.json(persons);
 });
+
+app.get("/persons/:id", (request, response) => {
+	const id = Number(request.params.id);
+	const person = persons.find(person => person.id === id);
+	if (person)
+		response.json(person);
+	else
+		response.status(404).end()
+});
