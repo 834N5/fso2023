c53039f598b5fcb9e8c56c9af0223a9579e187a6
834N5 <pkgduk@disroot.org>
2023-08-29 20:43:24 +1200

    Exercise 3.9 add cors
    thank you :3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
 
 const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 morgan.token("tinyData", (tokens, req, res) => {
 	return [
