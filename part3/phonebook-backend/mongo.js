90c7e880920165a99390b162335afa5116dec54a
834N5 <pkgduk@disroot.org>
2023-09-02 00:06:02 +1200

    Exercise 3.12

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,6 +1,34 @@
-require("dotenv").config()
-if (process.env.MONGO_PASS)
-	console.log(process.env.MONGO_PASS);
-else
-	console.log("MONGO_PASS is not set");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+if (!process.env.MONGO_URL) {
+	console.log("MONGO_URL is not set");
 	process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URL);
+const phonebookSchema = new mongoose.Schema({
+	name: String,
+	number: String
+});
+const Person = mongoose.model("Person", phonebookSchema)
+
+if (process.argv.length === 2) {
+	console.log("Phonebook:");
+	Person.find({}).then(result => {
+		result.forEach(person => {
+			console.log(person.name, person.number);
+		});
+		mongoose.connection.close()
+	})
+} else if (process.argv.length === 4) {
+	const person = new Person({
+		name: process.argv[2],
+		number: process.argv[3],
+	});
+
+	person.save().then(result => {
+		console.log(`Added "${result.name}", number: "${result.number}" to phonebook`);
+		mongoose.connection.close();
+	});
+}
