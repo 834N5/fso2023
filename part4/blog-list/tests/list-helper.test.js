a200a90969ddb3afbaaa9eec0c97908ced5c9fa7
834N5 <pkgduk@disroot.org>
2023-09-14 00:05:41 +1200

    Exercise 4.4

--- part4/blog-list/tests/list-helper.test.js
+++ part4/blog-list/tests/list-helper.test.js
@@ -5,3 +5,25 @@ test("Dummy returns 1", () => {
 	const result = listHelper.dummy(blogs);
 	expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+	const noBlogs = [];
+	const oneBlog = [
+		{
+			_id: "0-399-22690-7",
+			title: "The Very Hungry Caterpillar ",
+			author: "Eric Carle",
+			url: "https://www.goodreads.com/book/show/4948.The_Very_Hungry_Caterpillar",
+			likes: 1969,
+			__v: 0
+		}
+	];
+	test("Return 0 when there are no blogs", () => {
+		const result = listHelper.totalLikes(noBlogs);
+		expect(result).toBe(0);
+	});
+	test("Return the amount of likes in the singular blog when there is only 1 blog in a list", () => {
+		const result = listHelper.totalLikes(oneBlog);
+		expect(result).toBe(1969);
+	});
+});
--- part4/blog-list/utils/list-helper.js
+++ part4/blog-list/utils/list-helper.js
@@ -2,6 +2,12 @@ function dummy(blogs) {
 	return 1;
 }
 
+function totalLikes(blogs) {
+	const sum = blogs.reduce((acc, curr) => acc + curr.likes, 0);
+	return sum;
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 };
