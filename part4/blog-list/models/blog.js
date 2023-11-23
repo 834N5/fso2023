511342577a71010cd6e2addb523dacf5c83e57d0
834N5 <pkgduk@disroot.org>
2023-11-24 10:00:31 +1300

    toString() is a method

--- part4/blog-list/models/blog.js
+++ part4/blog-list/models/blog.js
@@ -9,7 +9,7 @@ const blogSchema = new mongoose.Schema({
 
 blogSchema.set("toJSON", {
 	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString;
+		returnedObject.id = returnedObject._id.toString();
 		delete returnedObject._id;
 		delete returnedObject.__v;
 	}
