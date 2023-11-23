438c62ebe751882ede6efb1b0edd1ebf8a96664d
834N5 <pkgduk@disroot.org>
2023-11-24 09:11:15 +1300

    Exercise 4.8 (I forgot to read)

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,19 +1,15 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (request, response) => {
-	Blog.find({})
-		.then(blogs => {
-			response.json(blogs);
-		});
+blogRouter.get("/", async (request, response) => {
+	const blogs = await Blog.find({})
+	response.json(blogs);
 });
 
-blogRouter.post("/", (request, response) => {
+blogRouter.post("/", async (request, response) => {
 	const blog = new Blog(request.body);
-	blog.save()
-		.then(result => {
-			response.status(201).json(result);
-		});
+	const result = await blog.save()
+	response.status(201).json(result);
 });
 
 module.exports = blogRouter;
