6dfb2cd9ec771b766703187c4a51421042b47d06
834N5 <pkgduk@disroot.org>
2024-01-03 14:54:08 +0900

    reduce risk of attacks and reduce amount of data mongoose has to process

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -7,7 +7,12 @@ blogRouter.get("/", async (request, response, next) => {
 });
 
 blogRouter.post("/", async (request, response, next) => {
-	const blog = new Blog(request.body);
+	const blog = new Blog({
+		"title": request.body.title,
+		"author": request.body.author,
+		"url": request.body.url,
+		"likes": request.body.likes
+	});
 	try {
 		const result = await blog.save()
 		response.status(201).json(result);
